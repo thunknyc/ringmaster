f8d922a003135823ee59fbc061206c88d1c2f2e0
Edwin Watkeys <edw@poseur.com>
2015-06-12 16:35:41 -0400

    fix whitespace

--- main.c
+++ main.c
@@ -32,9 +32,9 @@ typedef struct obstructor {
 
 unsigned long next_nat_pow_2(unsigned long n) {
   unsigned long x = 0;
-  
+
   if(!n--) return 1;
-  
+
   while(n) {
     n >>= 1;
     x = (x << 1) | 1;
@@ -48,19 +48,19 @@ unsigned long incr_index(obstructor *o, unsigned long i) {
 
 static obstructor *alloc_obstructor(unsigned long n_slots,
                                     short n_consumers) {
-  
+
   obstructor *o = malloc(sizeof(struct obstructor));
   assert(o != NULL);
-  
+
   o->slots = calloc(1, n_slots * sizeof(void*));
   assert(o->slots != NULL);
-  
+
   o->consumer_slots = calloc(1, n_consumers * sizeof(unsigned long));
   assert(o->consumer_slots);
-  
+
   o->threads = calloc(1, n_consumers * sizeof(pthread_t));
   assert(o->threads);
-  
+
   return o;
 }
 
@@ -68,12 +68,12 @@ obstructor *make_obstructor(unsigned long requested_slots,
                             short n_consumers,
                             consumer *consumers,
                             bool consumer_deps[n_consumers][n_consumers]) {
-  
+
   unsigned long n_slots = next_nat_pow_2(requested_slots);
-  
+
   obstructor *o = alloc_obstructor(n_slots, n_consumers);
   assert(o != NULL);
-  
+
   o->consumers = consumers;
   // o->threads
   // o->slots
@@ -85,7 +85,7 @@ obstructor *make_obstructor(unsigned long requested_slots,
   o->n_slots_mask = n_slots - 1;
   o->n_consumers = n_consumers;
   o->state = PAUSED;
-  
+
   return o;
 }
 
@@ -99,10 +99,10 @@ void start_obstructor(obstructor *o) {
     consumer_args *args = malloc(sizeof(consumer_args));
     assert(args != NULL);
     // Ownership of args is passed to consumer function.
-    
+
     args->o = o;
     args->consumer = i;
-    
+
     int result = pthread_create(&o->threads[i], NULL, o->consumers[i],
                                 (void *)args);
     assert(result == 0);
@@ -140,20 +140,20 @@ long long new_head(obstructor *o) {
 }
 
 void advance_tail(obstructor *o) {
-  
+
   // If buffer is empty do nothing.
   if (o->tail == o->head)
     return;
-  
+
   unsigned long is[o->n_consumers];
-  
+
   {
     unsigned long *isp = is;
     unsigned long *csp = o->consumer_slots;
     for (int i = 0; i < o->n_consumers; i++, isp++, csp++)
       *isp = (*csp + o->n_slots - o->tail) & o->n_slots_mask;
   }
-  
+
   unsigned long advance = o->n_slots_mask;
 
   {
@@ -162,20 +162,20 @@ void advance_tail(obstructor *o) {
       if (*isp < advance)
         advance = *isp;
   }
-  
+
   //  LOG("advance tail by %lu\n", advance);
   o->tail = (o->tail + advance) & o->n_slots_mask;
 }
 
 bool provide_obstructor(obstructor *o, void *v) {
-  
+
   advance_tail(o);
-  
+
   if (o->state != ACTIVE) return false;
-  
+
   long long h = new_head(o);
   if (h == -1) return false;
-  
+
   unsigned long new_head = (unsigned long)h;
   o->slots[o->head] = v;
   o->head = new_head;
@@ -190,7 +190,7 @@ long long datum_available(obstructor *o, unsigned short consumer) {
   unsigned long head = o->head;
   unsigned long slot = o->consumer_slots[consumer];
   int n_consumers = o->n_consumers;
-  
+
   if (head == slot) return -1;
 
   {
@@ -200,10 +200,10 @@ long long datum_available(obstructor *o, unsigned short consumer) {
     for (int j = 0; j < n_consumers; j++, cp++, dp++)
       if (*dp && *cp == slot) return -1;
   }
-  
+
   unsigned long new_slot = slot + 1 & o->n_slots_mask;
   o->consumer_slots[consumer] = new_slot;
-  
+
   return slot;
 }
 
@@ -258,23 +258,22 @@ int main(int argc, char **argv) {
                      {false, true, false}};
   consumer consumers[] = {echo_consumer, echo_consumer, echo_consumer};
   obstructor *o = make_obstructor(RING_BUFFER_SIZE, 3, consumers, deps);
-  
+
   assert(argc == 2);
   assert(o != NULL);
-  
+
   long iterations = atol(argv[1]);
   assert(iterations > 0);
-  
+
   LOG("iterations: %ld\n", iterations);
-  
+
   start_obstructor(o);
-  
+
   for (long i = 0; i < iterations; i++)
     provide_obstructor_block(o, (void *)i);
-  
+
   join_obstructor(o);
   destroy_obstructor(o);
-
 
   return 0;
 }
